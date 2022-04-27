90381271dc291dfd825e184a84b826190301a25a
Vitor Kilson <joaovitorkilson@gmail.com>
2022-04-26 22:25:07 -0300

    resolvi o bug no responsivo

--- script/specials_animation.js
+++ script/specials_animation.js
@@ -1,25 +1,25 @@
 export default function initSpecialAnimation() {
 
-}
-const dataSpecials = document.querySelectorAll('[data-special]');
-const dataSpecialsArray = Array.from(dataSpecials)
-
-let specialsList = document.querySelectorAll('.specials__list li');
-specialsList = Array.from(specialsList);
-
-
-
-specialsList.forEach((item, index) => {
+    const dataSpecials = document.querySelectorAll('[data-special]');
+    const dataSpecialsArray = Array.from(dataSpecials)
     
-    item.addEventListener('click', () => {
-        specialsList.forEach((li_item) => {
-            li_item.classList.remove('active')
-        })
-        item.classList.add('active')
-        dataSpecialsArray.forEach((div_item)=>{
-            div_item.classList.remove('active')
-        })
-        dataSpecialsArray[index].classList.add('active')
+    let specialsList = document.querySelectorAll('.specials__list li');
+    specialsList = Array.from(specialsList);
+    
+    
+    
+    specialsList.forEach((item, index) => {
         
-    })
-});
+        item.addEventListener('click', () => {
+            specialsList.forEach((li_item) => {
+                li_item.classList.remove('active')
+            })
+            item.classList.add('active')
+            dataSpecialsArray.forEach((div_item)=>{
+                div_item.classList.remove('active')
+            })
+            dataSpecialsArray[index].classList.add('active')
+            
+        })
+    });
+}
