803cc57ac082dd3bbb06e128e90b47b9d70d2ed5
Vitor Kilson <joaovitorkilson@gmail.com>
2022-04-26 22:38:12 -0300

    resolvi o bug no responsivo

--- script/script.js
+++ script/script.js
@@ -3,8 +3,8 @@ import initDishMenu from './dish__menu.js';
 import initDropDownMenu from './drop__down.js';
 import initHamburguerMenu from './hamburguer__menu.js';
 import initValidateSizeScroll from './navbar__animation.js';
-import initSpecialAnimation from './specials_animation.js'
-import initScrollSuave from './scroll_to.js'
+import initSpecialAnimation from './specials_animation.js';
+import initScrollSuave from './scroll_to.js';
 
 initCardAnimation();
 initValidateSizeScroll()
